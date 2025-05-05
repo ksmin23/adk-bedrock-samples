77d2d02c529f707553b8b3ed595907b5dfec2c9d
ksmin23 <sonagy23@gmail.com>
2025-05-05 22:12:33 +0900

    refactor codes

--- llm-auditor/llm_auditor/agent.py
+++ llm-auditor/llm_auditor/agent.py
@@ -12,8 +12,8 @@ from .sub_agents.reviser import reviser_agent
 llm_auditor = SequentialAgent(
   name="llm_auditor",
   description=(
-    'Evaluates LLM-generated answers, verifies actual accuracy using the'
-    ' web, and refines the response to ensure alignment with real-world'
+    'Evaluates LLM-generated answers, verifies actual accuracy using the web,'
+    ' and refines the response to ensure alignment with real-world'
     ' knowledge.'
   ),
   sub_agents=[critic_agent, reviser_agent]
